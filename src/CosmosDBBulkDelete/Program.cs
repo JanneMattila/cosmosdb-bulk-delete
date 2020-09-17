9a07fc82a0ff3c52c21204665c2a9a2ad27e432d
Janne Mattila <janne.mattila@hotmail.fi>
2020-09-17 17:43:53 +0300

    Add placeholder project

---
+++ src/CosmosDBBulkDelete/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosDBBulkDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
