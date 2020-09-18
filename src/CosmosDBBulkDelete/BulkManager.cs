652f74c2f9c8d60487ce5791cf4147cda66c574f
Janne Mattila <janne.mattila@hotmail.fi>
2020-09-18 13:47:03 +0300

    Add creating of stored procedures

--- src/CosmosDBBulkDelete/BulkManager.cs
+++ src/CosmosDBBulkDelete/BulkManager.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Scripts;
 using System;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace CosmosDBBulkDelete
@@ -16,6 +19,9 @@ namespace CosmosDBBulkDelete
         private const string DevicesContainer = "devices";
         private const string DeviceLocationsContainer = "devicelocations";
 
+        private const string BulkImport = "bulkImport";
+        private const string BulkCleanUp = "bulkCleanUp";
+
         public BulkManager(string connectionString)
         {
             _client = new CosmosClient(connectionString, new CosmosClientOptions() { AllowBulkExecution = true });
@@ -31,6 +37,7 @@ namespace CosmosDBBulkDelete
             Console.WriteLine($"Creating database {DatabaseName}...");
             _database = await _client.CreateDatabaseIfNotExistsAsync(DatabaseName, ThroughputProperties.CreateManualThroughput(400));
 
+            Console.WriteLine($"Creating container {DevicesContainer}...");
             _devicesContainer = await _database.DefineContainer(DevicesContainer, "/id")
                     .WithDefaultTimeToLive(TimeSpan.FromDays(7))
                     .WithIndexingPolicy()
@@ -43,6 +50,7 @@ namespace CosmosDBBulkDelete
                     .Attach()
                 .CreateIfNotExistsAsync();
 
+            Console.WriteLine($"Creating container {DeviceLocationsContainer}...");
             _deviceLocationsContainer = await _database.DefineContainer(DeviceLocationsContainer, "/deviceId")
                     .WithDefaultTimeToLive(TimeSpan.FromDays(7))
                     .WithIndexingPolicy()
@@ -54,6 +62,30 @@ namespace CosmosDBBulkDelete
                             .Attach()
                     .Attach()
                 .CreateIfNotExistsAsync();
+
+            await CreateStoredProcedure(BulkImport);
+            await CreateStoredProcedure(BulkCleanUp);
+        }
+
+        private async Task CreateStoredProcedure(string name)
+        {
+            Console.WriteLine($"Creating stored procedure {name}...");
+
+            try
+            {
+                await _deviceLocationsContainer.Scripts.CreateStoredProcedureAsync(new StoredProcedureProperties()
+                {
+                    Id = name,
+                    Body = File.ReadAllText($"{name}.js")
+                });
+            }
+            catch (CosmosException ex)
+            {
+                if (ex.StatusCode != HttpStatusCode.Conflict)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
